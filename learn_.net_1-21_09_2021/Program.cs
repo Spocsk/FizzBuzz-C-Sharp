553083217af1570f61fdf5a52b0a5a46882b5820
Dylan COUTO DE OLIVEIRA <dylan@MacBook-Air-de-Dylan.local>
2021-09-21 12:01:39 +0200

    Issou

--- learn_.net_1-21_09_2021/Program.cs
+++ learn_.net_1-21_09_2021/Program.cs
@@ -8,22 +8,20 @@ namespace learn_.net_1_21_09_2021
         {
             for(int i = 0; i <= 200; i++)
             {
-                if(i % 3 == 0 && i % 5 == 0)
+
+                Console.Write($"{i} ");
+
+                if(i % 3 == 0)
                 {
-                    Console.WriteLine($"{i} FizzBuzz");
+                    Console.Write("Fizz");
                 }
-                else if(i % 5 == 0)
+                if(i % 5 == 0)
                 {
-                    Console.WriteLine($"{i} Buzz");
+                    Console.Write("Buzz");
                 }
-                else if(i % 3 == 0)
-                {
-                    Console.WriteLine($"{i} Fizz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
+
+                Console.WriteLine("");
+
             }
         }
     }
