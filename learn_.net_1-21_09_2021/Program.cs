5220a1ce1a7a8d0346ad5291bda97620fbeb68c5
Dylan COUTO DE OLIVEIRA <dylan@MacBook-Air-de-Dylan.local>
2021-09-21 11:41:00 +0200

    Init commit

---
+++ learn_.net_1-21_09_2021/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace learn_.net_1_21_09_2021
+{
+    class Program
+    {
+        static void Main()
+        {
+            for(int i = 0; i <= 200; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+        }
+    }
+}
